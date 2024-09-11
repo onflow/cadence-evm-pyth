bcd01de874ffb837c5433359bcf582acf48123e5
Jerome P <frankly.watson@gmail.com>
2024-09-11 09:40:38 -0700

    Add test to confirm functionality on Solidity side

---
+++ solidity/test/TextExamplePythOracleForwarder.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import { Test, console2 } from "forge-std/Test.sol";
+import { ExamplePythOracleForwarder } from "../src/ExamplePythOracleForwarder.sol";
+import { MockPyth } from "@pythnetwork/pyth-sdk-solidity/MockPyth.sol";
+
+contract MyFirstPythContractTest is Test {
+    MockPyth public pyth;
+
+    ExamplePythOracleForwarder public app;
+
+    function setUp() public {
+        pyth = new MockPyth(60, 1);
+        app = new ExamplePythOracleForwarder(address(pyth));
+    }
+
+    function testGetGreeting() public view {
+        string memory expectedGreeting = "1.0.0"; // Replace with the expected greeting
+        string memory actualGreeting = app.getGreeting();
+        assertEq(actualGreeting, expectedGreeting, "Greeting does not match expected value");
+    }
+}
